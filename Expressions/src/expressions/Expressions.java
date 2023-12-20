058b86db7277c8d93947f55dd4a6b9bdbb5db5ac
Morteza363831 <120910275+Morteza363831@users.noreply.github.com>
2023-12-21 00:20:01 +0330

    Expressions project
    -- expression interface
    -- expressionImp class
    -- postFix()
    -- evaluator()
    -- postfixVar()
    -- evaluatorVar()

---
+++ Expressions/src/expressions/Expressions.java
@@ -0,0 +1,15 @@
+package expressions;
+
+public interface Expressions {
+    
+    public ExpressionsImp postFix();
+    
+    public ExpressionsImp evaluator();
+    
+    public ExpressionsImp inFix();
+    
+    public ExpressionsImp postFixVar();
+    
+    public void evaluatorVar();
+    
+}
